24ed6caaa0704903afc5db439e9ef979db10ecfb
Hikaru-Morita <ma20097@shibura-it.ac.jp>
2021-02-22 08:27:24 +0900

    matplotlib を用いたグラフ描画を追加

--- read_output.py
+++ read_output.py
@@ -1,13 +1,11 @@
-import json
-from collections import OrderedDict
-import pprint
-
 import pandas
+import matplotlib.pyplot as plt
 
+# 全体行数/2 を指定　chunksize*NUM_START=全体行数
 NUM_START = 50
 
 fname = './simulator/src/dist/output/individual_bpt.csv'
-reader = pandas.read_csv(fname, chunksize=100)
+reader = pandas.read_csv(fname, chunksize=300)
 
 if __name__ == "__main__":
     chunk_count = 0
@@ -15,9 +13,14 @@ if __name__ == "__main__":
     for chunk in reader:
         bpt_count = 0
         chunk_count = chunk_count + 1
-        print(chunk['500'])
+        print(chunk.iloc[2,:].values)
+        plot=chunk.iloc[2,:]
 
         # chunk's stracture is (100, 1002)
-        if chunk_count >= NUM_START:
+        if chunk_count == NUM_START:
             for bpt in chunk:
                 meadian_bpt = 1
+
+    fig = plt.figure()
+    plt.hist(plot,bins=20)
+    fig.savefig("test.png")
