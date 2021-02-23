a835fd1d3e7781e758c98de224be2e05e10b2682
Hikaru-Morita <ma20097@shibura-it.ac.jp>
2021-02-23 19:25:27 +0900

    参照元を追加

--- read_output.py
+++ read_output.py
@@ -3,35 +3,39 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # 全体行数/2 を指定　chunksize*NUM_START=全体行数
-threshold = 50
+threshold = 5
 
 if __name__ == "__main__":
     chunk_count = 0
     plot = []
 
+    average_fig = plt.figure()
+    average = []
+
     for para in range(1,10):
-        fname = './simulator/src/dist/output/individual/'+ str(para) +'.csv'
+        # fname = './simulator/src/dist/output/individual/'+ str(para) +'.csv'
+
+        # fname = '/home/moritta/Documents/simblock_result/shudo/default_2015/'+ str(para) +'.csv'
+        # fname = '/home/moritta/Documents/simblock_result/my/default_2015/'+ str(para) +'.csv'
+        fname = '/home/moritta/Documents/simblock_result/shudo/3times_2019/'+ str(para) +'.csv'
+        # fname = '/home/moritta/Documents/simblock_result/my/3times_2019/'+ str(para) +'.csv'
+
         reader = pandas.read_csv(fname, chunksize=50)
 
-
         for chunk in reader:
-            bpt_count = 0
             chunk_count = chunk_count + 1
             # print(chunk.iloc[10,:].values)
 
-            plot.append(np.average([int(i) for i in chunk.iloc[:,500].values]))
-
             # chunk's stracture is (chunksize, 1002)
-            if chunk_count >= threshold:
-                for bpt in chunk:
-                    meadian_bpt = 1
+            if chunk_count > threshold:
+                plot.append(np.average([int(i) for i in chunk.iloc[:,500].values]))
 
         # 各パラメータの中央値平均値
+        average.append(np.average(plot))
         print('para:{0}  {1}'.format(para,np.average(plot)))
 
-            # fig = plt.figure()
-            # plt.hist(plot,bins=20)
-            # fig.savefig("./simulator/src/dist/output/fig/"+ str(para)+".png")
+    plt.hist(plot)
+    average_fig.savefig("./simulator/src/dist/output/fig/average.png")      
 
     # plt.hist(plot)
     # fig.savefig("test2.png")
