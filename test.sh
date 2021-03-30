bb6e65cabaadbc61b66a52d1a772195d2e7933bf
Hikaru-Morita <ma20097@shibura-it.ac.jp>
2021-03-30 21:38:49 +0900

    修正

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-for para in `seq 1 10`; do
+for para in `seq 0 10`; do
     sed -i -e "14c public static double para_ = $para;" ./simulator/src/main/java/SimBlock/node/Score.java
     sudo gradle simulator:run
 done
